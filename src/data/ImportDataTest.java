7bff7d3aae892b94c6b2c3cab9886334a1c23ba4
trzytematyczna <monikrakoczy@gmail.com>
2015-03-26 23:46:52 +0100

    init

---
+++ src/data/ImportDataTest.java
@@ -0,0 +1,16 @@
+package data;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ImportDataTest {
+
+	@Test
+	public void test() {
+		ImportData im = new ImportData();
+		im.go();
+//		fail("Not yet implemented");
+	}
+
+}
---
+++ src/data/Main.java
@@ -0,0 +1,22 @@
+package data;
+
+public class Main {
+
+	
+	public static void main(String[] args){
+//		ImportData im  = new ImportData();
+//		im.go();
+		PageRankSlots prs = new PageRankSlots();
+		
+		String[] years= {"1999","2000","2001","2002"};
+		String[] months = {"-01%","-02%","-03%","-04%","-05%","-06%","-07%","-08%",
+				"-09%","-10%","-11%","-12%"};
+
+		for(String y : years){
+			for(String m : months){
+				System.out.println(y+m);
+					prs.go(y+m);
+			}
+		}
+	}
+}
